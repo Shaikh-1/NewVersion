e7b79548d7a2c665006769dbf900f39bde3a5685
shaikh1 <shaikh@kencloud.co.in>
2019-03-14 11:47:52 +0530

    3rd Commit

--- NewJenkProj/src/test/java/test1/Test1.java
+++ NewJenkProj/src/test/java/test1/Test1.java
@@ -2,6 +2,7 @@ package test1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -20,6 +21,12 @@ public class Test1 {
 	public void test() {
 		System.out.println("Test Successful");
 		System.out.println("Test Test");
+	}
+	
+	
+	@AfterMethod()
+	public void tearDown() {
+		System.out.println("Close");
 	}
 
 }
